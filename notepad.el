6137916bf785a2ac3ea02ef7570aa6fc6a18a4e1
Fabien Cazenave <fabien@cazenave.cc>
2012-03-04 00:44:29 +0100

    Ctrl-H = find/replace

--- notepad.el
+++ notepad.el
@@ -6,13 +6,13 @@
 ;| License       : WTFPL
 ;|
 ;| This configuration file should bring a Notepad-like behavior to Emacs.
-;| It reuses a small subset of ErgoEmacs but tries to remain as simple as possible.
+;| It reuses a subset of ErgoEmacs but tries to remain as simple as possible.
 ;|
-;| We're missing [F3] / Shift+[F3] to get the next/previous search occurrences.
-;| It would be nice if this configuration file could be activated/disactivated
-;| along with cua-mode.  If you know how to do that, please ping me!
-;|
-;| The undo/redo stack still sucks! This is the main issue we have to solve.
+;| Known problems:
+;|   * the undo/redo stack still sucks!
+;|   * [F3]/Shift+[F3] (next/previous search occurrences) still not implemented
+;|   * it would be nice if this configuration file could be activated /
+;|     disactivated along with cua-mode.
 ;|
 
 ;; CUA-Mode brings C-[z|x|c|v] shortcuts
@@ -20,7 +20,7 @@
 (require 'redo "redo.elc" t) ; for redo shortcut
 (global-set-key (kbd "C-S-z") 'redo)
 
-;; basic Notepad-like shortcuts
+;; Notepad-like shortcuts
 (defun close-frame ()
   "Closes the current frame or kills emacs if there are just one frame. It simulates the same functionality of the Close button in the frame title bar."
   (interactive)
@@ -34,10 +34,10 @@
 (global-set-key (kbd "C-S-s") 'write-file)         ; Save As...
 (global-set-key (kbd "C-a")   'mark-whole-buffer)  ; Select All
 (global-set-key (kbd "C-f")   'search-forward)     ; Find...
-(global-set-key (kbd "C-g")   'query-replace)      ; Find and replace...
+(global-set-key (kbd "C-h")   'query-replace)      ; Find and replace...
 (global-set-key (kbd "C-q")   'close-frame)        ; Quit
 
-;; text zoom
+;; Bonus: text zoom
 (defun text-scale-reset ()
   "Set the height of the default face in the current buffer to its default value."
   (interactive)
@@ -47,114 +47,116 @@
 (global-set-key (kbd "C-=")   'text-scale-increase)
 (global-set-key (kbd "C-0")   'text-scale-reset)
 
-;; selection with Shift + caret movement keys -- not working with Org-mode :-/
+;; Selection with Shift+[arrow] -- blatantly stolen from ErgoEmacs
+;; not working with Org-mode :-/
 (defun ergoemacs-fix-cua--pre-command-handler-1 ()
   "Fixes CUA minor mode so selection is highlighted only when Shift+<special key> is used (arrows keys, home, end, pgdn, pgup, etc.)."
- (defun cua--pre-command-handler-1 ()
-  ;; Cancel prefix key timeout if user enters another key.
-  (when cua--prefix-override-timer
-    (if (timerp cua--prefix-override-timer)
-        (cancel-timer cua--prefix-override-timer))
-    (setq cua--prefix-override-timer nil))
+  (defun cua--pre-command-handler-1 ()
+    ;; Cancel prefix key timeout if user enters another key.
+    (when cua--prefix-override-timer
+      (if (timerp cua--prefix-override-timer)
+          (cancel-timer cua--prefix-override-timer))
+      (setq cua--prefix-override-timer nil))
 
-  (cond
-   ;; Only symbol commands can have necessary properties
-   ((not (symbolp this-command))
-    nil)
+    (cond
+      ;; Only symbol commands can have necessary properties
+      ((not (symbolp this-command))
+        nil)
 
-   ;; Handle delete-selection property on non-movement commands
-   ((not (eq (get this-command 'CUA) 'move))
-    (when (and mark-active (not deactivate-mark))
-      (let* ((ds (or (get this-command 'delete-selection)
-                     (get this-command 'pending-delete)))
-             (nc (cond
-                  ((not ds) nil)
-                  ((eq ds 'yank)
-                   'cua-paste)
-                  ((eq ds 'kill)
-                   (if cua--rectangle
-                       'cua-copy-rectangle
-                     'cua-copy-region))
-                  ((eq ds 'supersede)
-                   (if cua--rectangle
-                       'cua-delete-rectangle
-                     'cua-delete-region))
-                  (t
-                   (if cua--rectangle
-                       'cua-delete-rectangle ;; replace?
-                     'cua-replace-region)))))
-        (if nc
-            (setq this-original-command this-command
-                  this-command nc)))))
+      ;; Handle delete-selection property on non-movement commands
+      ((not (eq (get this-command 'CUA) 'move))
+        (when (and mark-active (not deactivate-mark))
+          (let* ((ds (or (get this-command 'delete-selection)
+                         (get this-command 'pending-delete)))
+                 (nc (cond
+                      ((not ds) nil)
+                      ((eq ds 'yank)
+                       'cua-paste)
+                      ((eq ds 'kill)
+                       (if cua--rectangle
+                           'cua-copy-rectangle
+                         'cua-copy-region))
+                      ((eq ds 'supersede)
+                       (if cua--rectangle
+                           'cua-delete-rectangle
+                         'cua-delete-region))
+                      (t
+                       (if cua--rectangle
+                           'cua-delete-rectangle ;; replace?
+                         'cua-replace-region)))))
+            (if nc
+              (setq this-original-command this-command
+                    this-command nc)))))
 
-   ;; Handle shifted cursor keys and other movement commands.
-   ;; If region is not active, region is activated if key is shifted.
-   ;; If region is active, region is cancelled if key is unshifted
-   ;;   (and region not started with C-SPC).
-   ;; If rectangle is active, expand rectangle in specified direction and
-   ;;   ignore the movement.
-   ((if window-system
-        ;; Shortcut for window-system, assuming that input-decode-map is empty.
+      ;; Handle shifted cursor keys and other movement commands.
+      ;; If region is not active, region is activated if key is shifted.
+      ;; If region is active, region is cancelled if key is unshifted
+      ;;   (and region not started with C-SPC).
+      ;; If rectangle is active, expand rectangle in specified direction and
+      ;;   ignore the movement.
+      ((if window-system
+          ;; Shortcut for window-system, assuming that input-decode-map is empty.
 
-        ;; ErgoEmacs patch begin ------------------
-        ;;;; (memq 'shift (event-modifiers
-        ;;;;               (aref (this-single-command-raw-keys) 0)))
-        (and (memq 'shift (event-modifiers
-                           (aref (this-single-command-raw-keys) 0)))
-             ;; In this way, we expect to use CUA only with keys that
-             ;; are symbols (like <left>, <next>, etc.)
-             (symbolp (event-basic-type (aref (this-single-command-raw-keys) 0))))
-        ;; ErgoEmacs patch end --------------------
+          ;; ErgoEmacs patch begin ------------------
+          ;;;; (memq 'shift (event-modifiers
+          ;;;;               (aref (this-single-command-raw-keys) 0)))
+          (and (memq 'shift (event-modifiers
+                             (aref (this-single-command-raw-keys) 0)))
+               ;; In this way, we expect to use CUA only with keys that
+               ;; are symbols (like <left>, <next>, etc.)
+               (symbolp (event-basic-type (aref (this-single-command-raw-keys) 0))))
+          ;; ErgoEmacs patch end --------------------
 
-      (or
-       ;; Check if the final key-sequence was shifted.
-       (memq 'shift (event-modifiers
-                     (aref (this-single-command-keys) 0)))
-       ;; If not, maybe the raw key-sequence was mapped by input-decode-map
-       ;; to a shifted key (and then mapped down to its unshifted form).
-       (let* ((keys (this-single-command-raw-keys))
-              (ev (lookup-key input-decode-map keys)))
-         (or (and (vector ev) (memq 'shift (event-modifiers (aref ev 0))))
-             ;; Or maybe, the raw key-sequence was not an escape sequence
-             ;; and was shifted (and then mapped down to its unshifted form).
-             (memq 'shift (event-modifiers (aref keys 0)))))))
-    (unless mark-active
-      (push-mark-command nil t))
-    (setq cua--last-region-shifted t)
-    (setq cua--explicit-region-start nil))
+          (or
+            ;; Check if the final key-sequence was shifted.
+            (memq 'shift (event-modifiers
+                          (aref (this-single-command-keys) 0)))
+            ;; If not, maybe the raw key-sequence was mapped by input-decode-map
+            ;; to a shifted key (and then mapped down to its unshifted form).
+            (let* ((keys (this-single-command-raw-keys))
+                   (ev (lookup-key input-decode-map keys)))
+              (or (and (vector ev) (memq 'shift (event-modifiers (aref ev 0))))
+                  ;; Or maybe, the raw key-sequence was not an escape sequence
+                  ;; and was shifted (and then mapped down to its unshifted form).
+                  (memq 'shift (event-modifiers (aref keys 0)))))))
 
-   ;; Set mark if user explicitly said to do so
-   ((or cua--explicit-region-start cua--rectangle)
-    (unless mark-active
-      (push-mark-command nil nil)))
+        (unless mark-active
+          (push-mark-command nil t))
+        (setq cua--last-region-shifted t)
+        (setq cua--explicit-region-start nil))
 
-   ;; Else clear mark after this command.
-   (t
-    ;; If we set mark-active to nil here, the region highlight will not be
-    ;; removed by the direct_output_ commands.
-    (setq deactivate-mark t)))
+      ;; Set mark if user explicitly said to do so
+      ((or cua--explicit-region-start cua--rectangle)
+        (unless mark-active
+          (push-mark-command nil nil)))
 
-  ;; Detect extension of rectangles by mouse or other movement
-  (setq cua--buffer-and-point-before-command
-        (if cua--rectangle (cons (current-buffer) (point)))))
-)
+      ;; Else clear mark after this command.
+      (t
+        ;; If we set mark-active to nil here, the region highlight will not be
+        ;; removed by the direct_output_ commands.
+        (setq deactivate-mark t)))
 
-(defadvice kill-ring-save (before slick-copy activate compile)
-  "When called interactively with no active region, copy the current line."
-  (interactive
-   (if mark-active
-       (list (region-beginning) (region-end))
-     (progn
-       (message "Current line is copied.")
-       (list (line-beginning-position) (line-end-position)) ) ) ))
+    ;; Detect extension of rectangles by mouse or other movement
+    (setq cua--buffer-and-point-before-command
+      (if cua--rectangle (cons (current-buffer) (point))))))
+
+;; Bonus: cut/copy the current line when no selection is made
 (defadvice kill-region (before slick-copy activate compile)
   "When called interactively with no active region, cut the current line."
   (interactive
-   (if mark-active
-       (list (region-beginning) (region-end))
-     (progn
-       (message "Current line is cut.")
-       (list (line-beginning-position) (line-end-position)) ) ) ))
+    (if mark-active
+        (list (region-beginning) (region-end))
+      (progn
+        (message "Current line is cut.")
+        (list (line-beginning-position) (line-end-position)) ) ) ))
+(defadvice kill-ring-save (before slick-copy activate compile)
+  "When called interactively with no active region, copy the current line."
+  (interactive
+    (if mark-active
+        (list (region-beginning) (region-end))
+      (progn
+        (message "Current line is copied.")
+        (list (line-beginning-position) (line-end-position)) ) ) ))
 
 ;; frames
 ;(global-set-key (kbd "C-w")   'quit-window)
--- notepad.vim
+++ notepad.vim
@@ -8,20 +8,42 @@
 "| This configuration file should bring a Notepad-like behavior to Vim.
 "| It mostly relies on the built-in "mswin.vim" configuration.
 "|
+"| Note: Ctrl-S and Ctrl-Q are disabled on a few terminals (= xon/xoff)
+"|       like xterm, but they always work on gVim.
+"|
 
-" Use Ctrl-Q to quit and Ctrl+Shift+V for visual block selection
-noremap <C-Q> ZZ
-noremap <C-S-v> <C-v>
-
-" The so-called "MS Windows compatibility" settings are loaded by default on
+" The so-called "MS-Windows compatibility" settings are loaded by default on
 " MS-Windows, but have to be loaded explicitely on other OSes.
 source $VIMRUNTIME/mswin.vim
-behave mswin
 
-" Use Ctrl-s for saving, also in Insert mode
-noremap <C-s> :update<CR>
-vnoremap <C-s> <C-c>:update<CR>
-inoremap <C-s> <C-o>:update<CR>
+" Only difference: Ctrl-Q to quit and Ctrl+B for visual block selection
+noremap <C-q> ZZ
+noremap <C-b> <C-v>
+
+" Bonus: easy open/find/replace
+if has('gui_running')
+  " open
+  noremap  <C-o>      :browse open<CR>
+  vnoremap <C-o> <C-c>:browse open<CR>
+  inoremap <C-o> <C-o>:browse open<CR>
+  " search
+  noremap  <C-f>      :promptfind<CR>
+  vnoremap <C-f> <C-c>:promptfind<CR>
+  inoremap <C-f> <C-o>:promptfind<CR>
+  " replace
+  noremap  <C-h>      :promptrepl<CR>
+  vnoremap <C-h> <C-c>:promptrepl<CR>
+  inoremap <C-h> <C-o>:promptrepl<CR>
+else
+  " open
+  noremap  <C-o>      :edit .<CR>
+  vnoremap <C-o> <C-c>:edit .<CR>
+  inoremap <C-o> <C-o>:edit .<CR>
+  " search
+  noremap  <C-f>      /
+  vnoremap <C-f> <C-c>/
+  inoremap <C-f> <C-o>/
+endif
 
 " Bonus: +1|-1 with Ctrl-[j|k] (since Ctrl-a and Ctrl-x have be redefined...)
 noremap <C-j> <C-x>
